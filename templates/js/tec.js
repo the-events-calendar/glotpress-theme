e618976d3a8db36221a3367863e0f0fd52dd3a27
Zach Tirrell <zbtirrell@gmail.com>
2015-06-22 17:10:38 -0400

    add links to both mo and po

--- templates/js/tec.js
+++ templates/js/tec.js
@@ -1,14 +1,23 @@
 jQuery( document ).ready( function( $ ) {
-	$export_link = $( '#export' ).clone();
 
-	$heading = $( '.gp-content>h2' );
+	var $export_link = $( 'a#export' );
 
-	$export_link.prepend( '[' ).append( ']' );
-	$export_link.addClass('action').css( {
-		fontSize: '0.75em',
-		textDecoration: 'none',
-		textTransform: 'lowercase'
-	});
+	if ( $export_link.length ) {
 
-	$heading.append( $export_link );
+		export_url = $export_link.attr('href') + '?format=';
+		console.log( export_url);
+
+		$heading = $( '.gp-content>h2' );
+
+		$export_links = $('<span><em>export:</em> <a href="' + export_url + 'mo">po</a> | <a href="' + export_url + 'po">po</a></span>' );
+
+		$export_links.addClass('action').css( {
+			fontWeight: 'normal',
+			fontSize: '0.75em',
+			textDecoration: 'none',
+			textTransform: 'lowercase'
+		});
+
+		$heading.append( $export_links );
+	}
 });
